5591d54dc1a83d905ece0874907103df1a47aa95
Daniel Dymek <daniel@cherrypickgames.com>
2021-05-09 22:17:18 +0200

    bunch of terrible styling added, still no distance/kmph/load selector...

--- PROJECT-TRANSPORT/first.js
+++ PROJECT-TRANSPORT/first.js
@@ -1,12 +1,14 @@
-var values = {
-  dist: document.getElementById("dist"),
-  kmph: document.getElementById('kmph'),
-  load: document.getElementById('pkgWeight')
-}
-var dist = document.getElementById("dist").innerHTML;
-var kmph = values.kmph.value;
-var loadWeight = values.load.value; //kilograms
 
+
+var dist = 12;
+var kmph = 50;
+var loadWeight = 25000; //kilograms
+var distToEl = document.createElement("p")
+distToEl.innerHTML = "dystans do celu(A->B): " + dist + " km"
+var kmphToEl = document.createElement("p")
+kmphToEl.innerHTML = "srednia predkosc pojazdu: " + kmph + " kmph"
+var loadWeightToEl = document.createElement("p")
+loadWeightToEl.innerHTML = "zaladowanie pojazdu w kilogramach: " + loadWeight + " kg"
 var loading = Math.round(loadWeight * 0.0048);
 var unloading = Math.round(loadWeight * 0.0036);
 var distTime = (dist/kmph * 60);
@@ -16,21 +18,45 @@ var maxDriveTimeInOneRun = 270
 var maxDriveDaily = 540;
 var breakCount = Math.round(distTime / maxDriveTimeInOneRun);
 var breakTimePenalty = breakTime * breakCount;
-function driver() {
+var toAppend = document.body.getElementsByClassName("append")[0]
+var br = document.createElement("br")
+var dataEl = document.createElement("p")
+dataEl.innerHTML = "DANE: "
+var hadBeenRun = false;
+var startButton = document.getElementById('btn')
 
-  if(dist == 0 || kmph == 0 || loadWeight == 0){
-  alert("wprowadz poprawne dane")
-  console.log(dist);
-  console.log(kmph);
-  console.log(loadWeight);
+
+function driver() {
+  startButton.innerHTML = "ZATRZYMAJ"
+  startButton.style.backgroundColor = "RED"
+  startButton.style.disabled = "true"
+
+  if(hadBeenRun == true ){
+    window.location.reload(true)
   }else{
 
       var AtoBdriveWithBreakPen = distTime + breakTimePenalty;
       AtoBTotal = AtoBdriveWithBreakPen + loading + unloading;
       var BtoAdriveWithBreakPen = distTime + breakTimePenalty;
       BtoATotal = BtoAdriveWithBreakPen
-      var shouldSleepCountAB = Math.floor(AtoBTotal / maxDriveDaily)
-      var shouldSleepCountBA = Math.floor(BtoATotal / maxDriveDaily)
+      var shouldSleepCountAB = (AtoBTotal / maxDriveDaily)
+      console.log(shouldSleepCountAB);
+
+      if(shouldSleepCountAB >= 0.9){
+        shouldSleepCountAB = Math.round((AtoBTotal / maxDriveDaily))
+      }else{
+        shouldSleepCountAB = 0
+      }
+
+      var shouldSleepCountBA = (BtoATotal / maxDriveDaily)
+      console.log(shouldSleepCountBA)
+
+      if(shouldSleepCountBA >= 0.9){
+        shouldSleepCountBA = Math.round((BtoATotal / maxDriveDaily))
+      }else{
+        shouldSleepCountBA = 0
+      }
+
       var sleepAB = sleepTime * shouldSleepCountAB
       var sleepBA = sleepTime * shouldSleepCountBA
       if (AtoBTotal >= maxDriveDaily) {
@@ -43,14 +69,33 @@ function driver() {
           BtoATotal = BA
       }
       let sleepCombined = sleepBA + sleepAB
-      var total = BtoATotal + AtoBTotal
+      var total = BtoATotal + AtoBTotal + sleepCombined
+
+      setTimeout(function () {
+        toAppend.appendChild(dataEl)
+        toAppend.appendChild(distToEl)
+      }, 200);
+      setTimeout(function () {
+        toAppend.appendChild(kmphToEl)
+      }, 400);
+      setTimeout(function () {
+        toAppend.appendChild(loadWeightToEl)
+      }, 600);
+
+
+
 
       function timeAtoB(){
         var hrs = Math.floor(AtoBTotal / 60) + "h "
         var mins = Math.floor(AtoBTotal % 60) + "m"
         var penaltyHrs = Math.floor(breakTimePenalty / 60) + "h"
         var penaltyMins = Math.floor(breakTimePenalty % 60) + "m"
-        console.log("\x1b[35m" + penaltyHrs + penaltyMins + "\x1b[36m -> czas przerwy kierowcy przez podroz od A -> B")
+        // console.log("\x1b[35m" + penaltyHrs + penaltyMins + "\x1b[36m -> czas przerwy kierowcy przez podroz od A -> B")
+        var el = document.createElement("p");
+        el.textContent = "czas pracy(+przerwa/y) kierowcy od punktu A do punktu B: " + hrs + mins
+        setTimeout(function () {
+          toAppend.appendChild(el);
+        }, 1000);
         console.log("\x1b[35m" + hrs + mins + "\x1b[36m -> pelny czas jazdy kierowcy od punktu A do punktu B")
       }
       timeAtoB()
@@ -59,7 +104,12 @@ function driver() {
         var mins = Math.floor(BtoATotal % 60) + "m"
         var penaltyHrs = Math.floor(breakTimePenalty / 60) + "h"
         var penaltyMins = Math.floor(breakTimePenalty % 60) + "m"
-        console.log("\x1b[35m" + penaltyHrs + penaltyMins + "\x1b[36m -> czas przerwy kierowcy przez podroz od B -> A")
+        var el = document.createElement("p");
+        el.textContent = " czas pracy(+przerwa/y) kierowcy od punktu B do punktu A: " + hrs + mins
+        setTimeout(function () {
+          toAppend.appendChild(el);
+        }, 2000);
+        // console.log("\x1b[35m" + penaltyHrs + penaltyMins + "\x1b[36m -> czas przerwy kierowcy przez podroz od B -> A")
         console.log("\x1b[35m" + hrs + mins + "\x1b[36m -> pelny czas jazdy kierowcy od punktu B do punktu A")
       }
       timeBtoA()
@@ -67,14 +117,23 @@ function driver() {
         var hrs = Math.floor(total / 60) + "h "
         var mins = Math.floor(total % 60) + "m"
         console.log("\x1b[35m" + hrs + mins + "\x1b[36m -> Laczny czas podrozy")
+        var el = document.createElement("p");
+        el.textContent =  " pelny czas pracy kierowcy: "  + hrs + mins
+        setTimeout(function () {
+          toAppend.appendChild(el);
+          startButton.innerHTML = "WYCZYŚĆ"
+          startButton.style.backgroundColor = "ORANGE"
+        }, 3000);
       }
       totalTime()
       function driverSleepTime(){
         var hrs = Math.floor(sleepCombined / 60) + "h "
         var mins = Math.floor(sleepCombined % 60) + "m"
+
         console.log("\x1b[35m" + hrs + mins + "\x1b[36m -> Laczny czas spania/odpoczywania od jazdy")
       }
       driverSleepTime()
     }
+hadBeenRun = true;
 
     };
