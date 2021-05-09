d9dd9654831ccb7eb644ed6f81a7eada2267316f
Daniel Dymek <daniel@cherrypickgames.com>
2021-05-09 18:34:38 +0200

    trans

--- PROJECT-THIS_TLUMACZENIE/first.js
+++ PROJECT-THIS_TLUMACZENIE/first.js
@@ -24,42 +24,11 @@ callMethod1.sth.call(object1)
 console.log("\x1b[35m" + "advanced call");
 callMethod2.sth.call(object1)
 // NOTE: 2 bind methods, one simple, the second little more advanced
+// NOTE: bind - i do not really understand that
 
-   // NOTE: adds all randFunc() values together and multiplies this value by 5
-   function randFunc(maxRandomized){
-     for (var i = 0; i < 1; i++) {
-       let rand = Math.floor(Math.random() * maxRandomized + 1);
-       return rand
-     }
-   }
-   let bindMethod1 = {
-     sth: function () {
-      let arraySum = this.values6.reduce((a, b) => a + b, 0)
-      let result = arraySum * 5
-      console.log(result);
-     }
-   }
-   let bindMethod2 = {
-     sth: function () {
-       console.log(this);
-     }
-   }
+// NOTE: 2 apply methods, one simple, the second little more advanced
+// NOTE: apply - i do not really understand that
 
-    // NOTE: adds N  random values (0-100) to the values arr and then multiplies them by multiplierValue(not their count)
-    let bindMethod21 = {
-      sth: function (N, multiplierValue) {
-        for (var i = 0; i < n; i++) {
-          this.randomizedPool.push(randFunc(100))
-        }
-      }
-    }
-let object2 = {
-  values6: [randFunc(100),randFunc(100),randFunc(100),randFunc(100),randFunc(100)],
-  randomizedPool: []
-
-}
-bindMethod1.sth.call(object2);
-bindMethod2.sth.bind(object2);
 
 
 
--- PROJECT-TRANSPORT/first.js
+++ PROJECT-TRANSPORT/first.js
@@ -1,34 +1,48 @@
-var dist = 870;
-var kmph = 50;
-var loadWeight = 10020; //kilograms
+var values = {
+  dist: document.getElementById("dist"),
+  kmph: document.getElementById('kmph'),
+  load: document.getElementById('pkgWeight')
+}
+var dist = document.getElementById("dist").innerHTML;
+var kmph = values.kmph.value;
+var loadWeight = values.load.value; //kilograms
+
 var loading = Math.round(loadWeight * 0.0048);
 var unloading = Math.round(loadWeight * 0.0036);
 var distTime = (dist/kmph * 60);
 var breakTime = 45;
-var sleepTime = 540;
+var sleepTime = 660;
 var maxDriveTimeInOneRun = 270
 var maxDriveDaily = 540;
-var breakCount = Math.floor(distTime / maxDriveTimeInOneRun);
+var breakCount = Math.round(distTime / maxDriveTimeInOneRun);
 var breakTimePenalty = breakTime * breakCount;
 function driver() {
+
+  if(dist == 0 || kmph == 0 || loadWeight == 0){
+  alert("wprowadz poprawne dane")
+  console.log(dist);
+  console.log(kmph);
+  console.log(loadWeight);
+  }else{
+
       var AtoBdriveWithBreakPen = distTime + breakTimePenalty;
       AtoBTotal = AtoBdriveWithBreakPen + loading + unloading;
       var BtoAdriveWithBreakPen = distTime + breakTimePenalty;
       BtoATotal = BtoAdriveWithBreakPen
-      var shouldSleepCount = Math.floor(AtoBTotal / maxDriveDaily)
-      var sleep = sleepTime * shouldSleepCount
-
+      var shouldSleepCountAB = Math.floor(AtoBTotal / maxDriveDaily)
+      var shouldSleepCountBA = Math.floor(BtoATotal / maxDriveDaily)
+      var sleepAB = sleepTime * shouldSleepCountAB
+      var sleepBA = sleepTime * shouldSleepCountBA
       if (AtoBTotal >= maxDriveDaily) {
 
-        var AB = (AtoBTotal = AtoBTotal +  sleep)
+        var AB = (AtoBTotal = AtoBTotal +  sleepAB)
           AtoBTotal = AB
       }
       if (BtoATotal >= maxDriveDaily) {
-        console.log(sleep)
-        var BA = (BtoATotal = BtoATotal +  sleep)
-        console.log()
+        var BA = (BtoATotal = BtoATotal +  sleepBA)
           BtoATotal = BA
       }
+      let sleepCombined = sleepBA + sleepAB
       var total = BtoATotal + AtoBTotal
 
       function timeAtoB(){
@@ -56,13 +70,11 @@ function driver() {
       }
       totalTime()
       function driverSleepTime(){
-        var sleep = sleepTime * shouldSleepCount
-        var hrs = Math.floor(sleep / 60) + "h "
-        var mins = Math.floor(sleep % 60) + "m"
-        console.log("\x1b[35m" + hrs + mins + "\x1b[36m -> Laczny czas spania")
+        var hrs = Math.floor(sleepCombined / 60) + "h "
+        var mins = Math.floor(sleepCombined % 60) + "m"
+        console.log("\x1b[35m" + hrs + mins + "\x1b[36m -> Laczny czas spania/odpoczywania od jazdy")
       }
       driverSleepTime()
+    }
 
     };
-
-driver();
