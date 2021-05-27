7b5c286d60a3a07104e05fd9d6e3fd83a306bd2c
Daniel Dymek <daniel@cherrypickgames.com>
2021-05-28 00:49:15 +0200

    clicker

--- PROJECT-clicker/first.js
+++ PROJECT-clicker/first.js
@@ -11,8 +11,8 @@ function navContact(){
     window.location.href = 'indexKontakt.html'
 }
 
-let credits = 25;
-let creditsPerClick = 1;
+let credits = 0;
+let creditsPerClick = 4.494229783818834e+99;
 let upgradeStep = 0;
 let upgradeCosts = [5];
 let idleValue = 0;
@@ -24,7 +24,6 @@ let clickpower = 0
 let idleCreditsBuffor = 0;
 let idleBufforLimits = [50];
 let idleBufforLimitLevel = 0;
-
 
 //default data
   var balanceString = "Total clicks: " + credits;
@@ -74,15 +73,50 @@ setInterval(function () {
   var autoClickerUpgradeCostString = "Autoclicker levelup cost: " + idleUpgradeCosts[idleUpgradeStep];
   var autoClickerVaultString = "Clicks vault: " + idleCreditsBuffor;
     if(idleValue > 0){
-      if(idleCreditsBuffor > idleBufforLimits[idleBufforLimitLevel]){
-        idleCreditsBuffor = idleCreditsBuffor + idleValue
-        idleBufforElement.style.backgroundColor = "transparent"
-        idleBufforElement.textContent = autoClickerVaultString
+        if(idleCreditsBuffor < idleBufforLimits[idleBufforLimitLevel]){
+          idleCreditsBuffor = idleCreditsBuffor + idleValue
+          idleBufforElement.style.backgroundColor = "transparent"
+          idleBufforElement.textContent = autoClickerVaultString
+        }
       }
 
+}, interval);
+function valueConverter(){
+    if (credits >= 1000 && credits <= 999999) {
+      balanceString = "Total clicks: " + (credits / 1000).toFixed(1) + "k"
+      balanceElement.textContent = balanceString;
+      console.log("one");
     }
+    if (Math.round(credits / 1000) == 1000) {
+      balanceString = "Total clicks: " + (credits / 1000000).toFixed(1) + "m"
+      balanceElement.textContent = balanceString;
+      console.log("two");
 
+    }
+    if (credits >= 1000000 && credits <= 999999999) {
+      balanceString = "Total clicks: " + (credits / 1000000).toFixed(1) + "m"
+      balanceElement.textContent = balanceString;
+      console.log("three");
 
+    }
+    if (Math.round(credits / 100000) > 1000) {
+      balanceString = "Total clicks: " + (credits / 100000000).toFixed(1) + "b"
+      balanceElement.textContent = balanceString;
+      console.log("four");
+
+    }
+    if (credits >= 1000000000 && credits <= 1000000000 ) {
+      balanceString = "Total clicks: " + (credits / 100000000).toFixed(1) + "b"
+      balanceElement.textContent = balanceString;
+      console.log("five");
+
+    }
+    // var balanceString = "Total clicks: " + credits
+}
+valueConverter()
+
+setInterval(function () {
+  // valueConverter()
 }, interval);
 
 function genClickUpgradeCosts() {
@@ -95,7 +129,7 @@ function genIdleUpgradeCosts() {
 }
 
 function genIdleBufforLimit() {
-  idleBufforLimits.push(Math.floor(idleBufforLimits[idleBufforLimitLevel] * 1.2))
+  idleBufforLimits.push(Math.round(idleBufforLimits[idleBufforLimitLevel - 1] * 2.1))
 }
 
 function a() { //clicker
@@ -107,6 +141,7 @@ function a() { //clicker
   var autoClickerUpgradeCostString = "Autoclicker levelup cost: " + idleUpgradeCosts[idleUpgradeStep];
   var autoClickerVaultString = "Clicks vault: " + idleCreditsBuffor;
   balanceElement.textContent = balanceString
+  valueConverter()
 
 }
 function b() { // clickAmountUpgrade
@@ -128,6 +163,8 @@ function b() { // clickAmountUpgrade
     balanceElement.textContent = balanceString
     clickUpgradeCostElement.textContent = clicksUpgradeCostString
     creditsPerClickElement.textContent = clicksPerClickString
+    valueConverter()
+
 
     setTimeout(function () {
       creditsPerClickElement.style.backgroundColor = "green"
@@ -170,13 +207,8 @@ function c() { //idle upgrade
     credits = credits - idleUpgradeCosts[idleUpgradeStep]
     idleBufforLimitLevel++
     idleUpgradeStep++
-<<<<<<< HEAD
     idlepower++
-=======
-    power++
-    idleBufforLimitLevel++
     genIdleBufforLimit()
->>>>>>> 41278fa29a20686def1980b602c8fd104a972cda
     midContentElement.appendChild(idleValueElement)
     midContentElement.appendChild(idleBufforElement)
     idleValue = idleValue + 1 + (Math.pow(2, idlepower))
@@ -190,6 +222,8 @@ function c() { //idle upgrade
     idleValueElement.textContent = autoClickerLevelString
     idleUpgradeElement.textContent = "Autoclicker levelup cost: " + idleUpgradeCosts[idleUpgradeStep];
     upgradeIdleButton.textContent = "Upgrade Autoclicker"
+    valueConverter()
+
 
     if (idleUpgradeStep > 1) {
       setTimeout(function () {
@@ -233,7 +267,7 @@ function c() { //idle upgrade
       else{
     idleUpgradeElement.textContent = "Autoclicker levelup cost: " + idleUpgradeCosts[idleUpgradeStep];
   }
-  if(idleUpgradeStep == 1 || isUpgraded == false){
+  if(idleUpgradeStep == 0 || isUpgraded == false){
     recieveIdleButton.style.visibility = "visible";
 
 
@@ -285,6 +319,8 @@ function d() {
   var autoClickerUpgradeCostString = "Autoclicker levelup cost: " + idleUpgradeCosts[idleUpgradeStep];
   var autoClickerVaultString = "Clicks vault: " + idleCreditsBuffor;
   balanceElement.textContent = balanceString
+  valueConverter()
+
   if (idleUpgradeStep > 0) {
     idleBufforElement.textContent = autoClickerVaultString
   }
